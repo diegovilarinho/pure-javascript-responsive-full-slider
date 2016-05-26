51ef4d45177d252ffce7a6ace931d53dc8de6155
Diego Vilarinho <diegovilarinhoo@gmail.com>
2016-05-26 02:49:51 -0300

    Adicionadas funções de paginação, navegação prev/next, autoplay e stopOnMouseOver

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Pure JavaScript Full Image Slider
-<!-- [Demo](http://diegovilarinho.com.br/diego-vilarinho-theme) -->
+<!-- [Demo](http://diegovilarinho.com.br/pure-js-full-slider-demo) -->
 
-Projeto inicial de criação de full slider responsivo  utilizando javascript puro(HTML e CSS também, claro :D).
+Full slider com navegação prev/next e paginação(que também pode ser utilizada como bullets) utilizando javascript puro.
--- src/fullSlider.js
+++ src/fullSlider.js
@@ -1,58 +1,137 @@
-window.onload = function () {
+(function() {
   
-  var slider = document.getElementsByClassName('slider')[0],
-      images = slider.getElementsByTagName('img'),
-      counter = 0,
-      nextBtn = slider.getElementsByClassName('next')[0],
-      prevBtn = slider.getElementsByClassName('prev')[0];
-  
-  
-  function showImage (index) {
-    // Remove all classnames on the images (so hide them)
-    for (var i = 0; i < images.length; i += 1) {
-      images[i].className = 'hideImage';
-    }
-    
-    // Add the showImage classname to the img-element you want
-    images[index].className = 'showImage';
+  function Slideshow( element ) {
+    this.el = document.querySelector( element );
+    this.init();
   }
   
-  
-  function nextImg () {
-    // counter variable gets the current img selected
-    // if we have the last img, we switch back to the first one again
-    
-    if (counter < images.length -1) {
-      counter += 1;
-    } else {
-      counter = 0;
-    }
+  Slideshow.prototype = {
+    init: function() {
+      this.wrapper = this.el.querySelector( ".slider__wrapper" );
+      this.slides = this.el.querySelectorAll( ".slide" );
+      this.previous = this.el.querySelector( ".slider__nav--prev" );
+      this.next = this.el.querySelector( ".slider__nav--next" );
+      this.navigationLinks = this.el.querySelectorAll( ".slider__pagination a" );
+      this.index = 0;
+      this.total = this.slides.length;
+      this.timer = null;
+      
+      this.setup();
+      this.actions();
+    },
 
-    showImage(counter);
-  }
-  
-  function prevImg () {
-    // counter variable gets the current img selected
-    // if we have the first img, we switch back to the last one again
-    
-    if (counter > 0) {
-      counter -= 1;
-    } else {
-      counter = images.length - 1;
-    }
+    _slideTo: function( slide ) {
+      var currentSlide = this.slides[slide];
+      currentSlide.style.opacity = 1;
+      
+      for( var i = 0; i < this.slides.length; i++ ) {
+        var slide = this.slides[i];
+        if( slide !== currentSlide ) {
+          slide.style.opacity = 0;
+        }
+      }
+    },
 
-    showImage(counter);
-  }
+    _highlightCurrentLink: function( link ) {
+      var self = this;
+      for( var i = 0; i < self.navigationLinks.length; ++i ) {
+        var a = self.navigationLinks[i];
+        a.className = "";           
+      }
+      link.className = "is-current"; 
+    },
+
+    setup: function() {
+      var self = this;
+      
+      for( var l = 0; l < self.slides.length; ++l ) {
+        var elSlide = self.slides[l];
+        var image = elSlide.getAttribute( "data-image" );
+        elSlide.style.backgroundImage = "url(" + image + ")";
+      }
+      
+      for( var k = 0; k < self.navigationLinks.length; ++k ) {
+        var pagLink = self.navigationLinks[k];
+        pagLink.setAttribute( "data-index", k );
+      } 
+    },
+
+    actions: function() {
+      var self = this;
+      
+      self.next.addEventListener( "click", function() {
+        self.index++;
+        
+        if( self.index == self.total ) {
+          self.index = 0;
+        }
+        
+        self._slideTo( self.index );
+        self._highlightCurrentLink( self.navigationLinks[self.index] );
+      }, false);
+      
+      self.previous.addEventListener( "click", function() {
+        self.index--;
+        
+        if( self.index == -1 ) {
+          self.index = self.total - 1;
+        }
+        
+        self._slideTo( self.index );
+        self._highlightCurrentLink( self.navigationLinks[self.index] );
+      }, false);
+      
+      for( var i = 0; i < self.navigationLinks.length; ++i ) {
+        var a = self.navigationLinks[i];
+        a.addEventListener( "click", function( e ) {
+          e.preventDefault();
+          var n = parseInt( this.getAttribute( "data-index" ), 10 );
+          self.index = n; 
+          
+          if( self.index == 0 ) {
+            self.index = 0;
+          }
+          
+          if( self.index == self.total - 1 ) {
+            self.index = self.total - 1;
+          }
+                  
+          self._slideTo( self.index );
+          self._highlightCurrentLink( this );
+        }, false);
+      }
+    },
+
+    autoplay: function( time ) {
+      if( time === undefined ) {
+        time = 4000;
+      }
+      var self = this;
+      self.timer = setInterval(function() {
+        self.index++;
+        if( self.index == self.slides.length ) {
+          self.index = 0;
+        }
+        self._slideTo( self.index );
+      }, time);
+    },
+
+    stopStart: function() {
+      var self = this;
+      self.el.addEventListener( "mouseover", function() {
+        clearInterval( self.timer );
+        self.timer = null;
+      }, false);
+
+      self.el.addEventListener( "mouseout", function() {
+        self.action();
+      }, false);
+    }
+  };
   
-//  If you want to have an auto-slider, uncomment this: 
-//  window.setInterval(prevImg, 2000);
-  
-  // give the buttons an onclick event
-  nextBtn.onclick = nextImg;
-  prevBtn.onclick = prevImg;
+  document.addEventListener( "DOMContentLoaded", function() {
+    var sliderA = new Slideshow( "#main-slider" );
+  });
   
   
-  // start it
-  showImage(counter);
-  
-};
+})();
